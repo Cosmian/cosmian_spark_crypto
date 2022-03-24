7557287a699391961fde135c1a7ff809882af92c
Bruno Grieder <bruno.grieder@cosmian.com>
2022-03-24 19:48:02 +0100

    encryption and decruption properties

--- src/main/java/com/cosmian/spark/CosmianAbeFactory.java
+++ src/main/java/com/cosmian/spark/CosmianAbeFactory.java
@@ -8,21 +8,36 @@ import com.cosmian.rest.abe.acccess_policy.Attr;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.crypto.DecryptionKeyRetriever;
+import org.apache.parquet.crypto.DecryptionPropertiesFactory;
 import org.apache.parquet.crypto.EncryptionPropertiesFactory;
+import org.apache.parquet.crypto.FileDecryptionProperties;
 import org.apache.parquet.crypto.FileEncryptionProperties;
+import org.apache.parquet.crypto.KeyAccessDeniedException;
 import org.apache.parquet.crypto.ParquetCipher;
 import org.apache.parquet.crypto.ParquetCryptoRuntimeException;
 import org.apache.parquet.hadoop.api.WriteSupport.WriteContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EncryptionPropertiesFactoryImpl implements EncryptionPropertiesFactory {
+public class CosmianAbeFactory implements EncryptionPropertiesFactory, DecryptionPropertiesFactory {
+
+    public CosmianAbeFactory() {
+        // System.out.println("CosmianEncryptionFactory instantiation on " +
+        // Thread.currentThread().getName());
+    }
 
     /**
      * The handle of the encryption cache created with
      * {@link Ffi#createEncryptionCache(com.cosmian.rest.kmip.objects.PublicKey)}
      */
     public static final String COSMIAN_ENCRYPTION_CACHE_HANDLE = "cosmian.encryption.cache.handle";
+
+    /**
+     * The handle of the decryption cache created with
+     * {@link Ffi#createDecryptionCache(com.cosmian.rest.kmip.objects.PrivateKey)}
+     */
+    public static final String COSMIAN_DECRYPTION_CACHE_HANDLE = "cosmian.decryption.cache.handle";
 
     /**
      * The policy attributes to use for that file for instance:
@@ -33,11 +48,14 @@ public class EncryptionPropertiesFactoryImpl implements EncryptionPropertiesFact
      */
     public static final String COSMIAN_ENCRYPTION_ATTRIBUTES = "cosmian.encryption.attributes";
 
-    private static final Logger LOG = LoggerFactory.getLogger(EncryptionPropertiesFactoryImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CosmianAbeFactory.class);
 
     @Override
     public FileEncryptionProperties getFileEncryptionProperties(Configuration fileHadoopConfig, Path tempFilePath,
             WriteContext fileWriteContext) throws ParquetCryptoRuntimeException {
+
+        // System.out.println("Name: " + fileWriteContext.getClass().getName() + ",
+        // path: " + tempFilePath);
 
         // Recover the encryption cache handle for the given public key
         int encryptionCacheHandle = fileHadoopConfig.getInt(COSMIAN_ENCRYPTION_CACHE_HANDLE, -1);
@@ -78,10 +96,48 @@ public class EncryptionPropertiesFactoryImpl implements EncryptionPropertiesFact
         // ... and the meta_data is the actual encryption of the key
         byte[] footerKeyMetadata = header.getEncryptedHeaderBytes();
 
+        // System.out.println("...Encrypted with attributes: " + String.join(",",
+        // attributeStrings));
+
         FileEncryptionProperties.Builder propertiesBuilder = FileEncryptionProperties.builder(footerKeyBytes)
                 .withFooterKeyMetadata(footerKeyMetadata)
                 .withAlgorithm(cipher);
         // .withEncryptedColumns(encryptedColumns);
+        return propertiesBuilder.build();
+    }
+
+    @Override
+    public FileDecryptionProperties getFileDecryptionProperties(Configuration hadoopConfig, Path filePath)
+            throws ParquetCryptoRuntimeException {
+
+        // System.out.println("Decrypting file: " + filePath);
+
+        // Recover the decryption cache handle for the given public key
+        int decryptionCacheHandle = hadoopConfig.getInt(COSMIAN_DECRYPTION_CACHE_HANDLE, -1);
+        if (decryptionCacheHandle == -1) {
+            LOG.debug("No Cosmian Decryption cache handle available. Ignoring decryption");
+            return null;
+        }
+
+        FileDecryptionProperties.Builder propertiesBuilder = FileDecryptionProperties.builder()
+                .withKeyRetriever(new DecryptionKeyRetriever() {
+
+                    @Override
+                    public byte[] getKey(byte[] keyMetaData)
+                            throws KeyAccessDeniedException, ParquetCryptoRuntimeException {
+
+                        try {
+                            return Ffi.decryptHeaderUsingCache(decryptionCacheHandle, keyMetaData).getSymmetricKey();
+                        } catch (FfiException | CosmianException e) {
+                            LOG.error("Header decryption failed on file {}: {}", filePath, e.getMessage());
+                            throw new ParquetCryptoRuntimeException("Header decryption failed: " +
+                                    e.getMessage(), e);
+                        }
+
+                    }
+
+                })
+                .withPlaintextFilesAllowed();
         return propertiesBuilder.build();
     }
 
